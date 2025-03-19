bb3f992e04d43f5a52984922db450793f11bae28
mans ahmadian <mans.ahmadian@gmail.com>
2025-03-18 20:14:57 -0700

    first application that uses semantic kernel and Ollama connector to get response.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient
+            {
+                Timeout = TimeSpan.FromMinutes(60), // Increase as needed
+                BaseAddress = new Uri("http://localhost:11434")
+            };
+            var builder = Kernel.CreateBuilder();
+            builder.AddOllamaChatCompletion("PHI4:Latest",  httpClient: client);
+
+            var kernel = builder.Build();
+            var chatService = kernel.GetRequiredService<IChatCompletionService>();
+
+            var history = new ChatHistory();
+            history.AddSystemMessage("You are a helpful assistant.");
+            string userMessage = "why the sky is blue, present detail description in around 3000 words";
+            history.AddUserMessage(userMessage);
+
+            var response = chatService.GetChatMessageContentAsync(history).GetAwaiter().GetResult();
+
+            Console.WriteLine($"Bot: {response.Content}");
+
+            history.AddMessage(response.Role, response.Content ?? string.Empty);
+
+        }
+    }
+}
